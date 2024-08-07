3ffb1a1c8d75f4b6c9957a48bb46552e120c2f3c
Rémi Genet <71651541+remigenet@users.noreply.github.com>
2024-08-07 14:44:34 +0200

    Handle other standard shell than bash
    scan for commonly used shell configuration files

--- install.py
+++ install.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-
 import os
 import sys
 import shutil
 from datetime import datetime
-
 
 def backup_file(file):
     if os.path.isfile(file):
@@ -12,17 +10,37 @@ def backup_file(file):
         save_to = file + '_' + backup_postfix + '.bak'
         shutil.copyfile(file, save_to)
 
+def add_to_rc_file(file_path, content):
+    if os.path.isfile(file_path):
+        with open(file_path, 'r+') as file:
+            if content not in file.read():
+                file.write(content)
 
 if __name__ == "__main__":
     script_dir = os.path.abspath(os.path.dirname(sys.argv[0]))  # '~/.local/share/git_switcher'
-    rc_file = os.path.expanduser('~/.bashrc')
+    
+    # List of common shell configuration files
+    rc_files = [
+        '~/.bashrc',
+        '~/.zshrc',
+        '~/.bash_profile',
+        '~/.profile',
+        '~/.login',
+        '~/.zprofile'
+    ]
 
-    backup_file(rc_file)
+    # Backup and modify each existing rc file
+    for rc_file in rc_files:
+        rc_file_path = os.path.expanduser(rc_file)
+        if os.path.isfile(rc_file_path):
+            backup_file(rc_file_path)
+            
+            need_add = f"\nif [ -d '{ script_dir }' ]; then\n" \
+                       f"    alias git_switcher='{ script_dir }/source/git_switcher.py'\n" \
+                       f"    alias git_switcher_uninstall='{ script_dir }/uninstall.py'\n" \
+                       f"fi\n"
+            
+            add_to_rc_file(rc_file_path, need_add)
+
+    # Backup SSH config
     backup_file(os.path.expanduser('~/.ssh/config'))
-
-    need_add = f"if [ -d '{ script_dir }' ]; then alias git_switcher='{ script_dir }/source/git_switcher.py'; " + \
-               f"alias git_switcher_uninstall='{ script_dir }/uninstall.py'; fi"
-
-    with open(rc_file, 'r+') as file:
-        if need_add not in file.read():
-            file.write(need_add)
